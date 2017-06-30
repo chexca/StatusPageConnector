8f3fe2cbaeae5c051b9294c8eccba4c2ef6bca16
Christian Exposito <chexca20@gmail.com>
2017-06-30 17:23:01 +0200

    Adding setup files

---
+++ status_page_connector/setup.py
@@ -0,0 +1,15 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'status_page_connector',
+    packages = ['status_page_connector'],
+    version = 'v0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Tool for connecting with Status Page API',
+    author = 'Christian Exposito',
+    author_email = 'chexca20@gmail.com',
+    url = 'https://github.com/chexca/StatusPageConnector',
+    download_url = 'https://github.com/chexca/StatusPageConnector/archive/v0.1.tar.gz',
+    keywords = ['StatusPage', 'StatusPageAPI', 'StatusPageConnector']
+    classifiers = [],
+)
